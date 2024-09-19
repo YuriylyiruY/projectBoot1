b95e788081953175fea19496c2a4f6cedeae5afd
komen <ykomendantskiy@list.ru>
2024-09-19 22:24:56 +0400

    Commit

--- src/main/java/buteApp/projectBoot/controllers/PeopleController.java
+++ src/main/java/buteApp/projectBoot/controllers/PeopleController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
 @Controller
 @RequestMapping("/")
 public class PeopleController {
